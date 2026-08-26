7acb3339ab4cbe5113f8dd3641f4ba76abbc7f89
HAEKWANG DAN <danhk0612@gmail.com>
2026-08-26 13:15:57 +0900

    chore: complete 0.1 validation and begin 0.2

--- internal/app/app.go
+++ internal/app/app.go
@@ -7,7 +7,7 @@ import (
 	"os"
 )
 
-const Version = "0.1.0-dev"
+const Version = "0.2.0-dev"
 
 func Run(args []string) error {
 	return run(args, os.Stdout)
@@ -15,7 +15,7 @@ func Run(args []string) error {
 
 func run(args []string, output io.Writer) error {
 	if len(args) == 0 {
-		_, err := fmt.Fprintf(output, "DKDrive %s\n0.1 기술 검증 단계: 아직 드라이브를 마운트하지 않습니다.\n", Version)
+		_, err := fmt.Fprintf(output, "DKDrive %s\n0.2 SFTP 기본 기능 개발 단계입니다.\n", Version)
 		return err
 	}
 
